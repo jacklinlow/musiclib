f3f3eb448cfcb00794e2b42c3541b21c8944532c
jacklinlow <jacklinlow@hotmail.co.uk>
2016-04-10 18:15:31 +0100

    first commit

---
+++ src/main/java/com/jacklin/MusiclibApplication.java
@@ -0,0 +1,24 @@
+package com.jacklin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
+
+@SpringBootApplication
+@RestController
+@Configuration
+@ComponentScan
+public class MusiclibApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MusiclibApplication.class, args);
+	}
+}
---
+++ src/main/java/com/jacklin/SongRepository.java
@@ -0,0 +1,16 @@
+
+package com.jacklin;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.web.bind.annotation.CrossOrigin;
+
+import java.util.List;
+
+@RepositoryRestResource(collectionResourceRel = "song", path = "song")
+public interface SongRepository extends MongoRepository<Song, String> {
+
+	List<Song> findByArtist(@Param("artist") String artist);
+
+}
