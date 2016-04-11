117a8e2e61b1907b556821597f59e1d465476a46
jacklinlow <jacklinlow@hotmail.co.uk>
2016-04-11 22:24:56 +0100

    Add form to add songs via rest POST

---
+++ target/classes/static/js/app.js
@@ -0,0 +1,75 @@
+var myApp = angular.module('myApp', []);
+//["ngResource", "spring-data-rest"]);
+
+myApp.config(function($httpProvider) {
+  $httpProvider.defaults.useXDomain = true;
+  delete $httpProvider.defaults.headers.common['X-Requested-With'];
+});
+
+myApp.controller('mainController', ['$scope', '$filter', '$http', function ($scope, $filter, $http) {
+
+    $scope.handle = '';
+
+    $scope.lowercasehandle = function () {
+        return $filter('lowercase')($scope.handle);
+    };
+
+    $scope.characters = 5;
+
+//    var httpPromise = $http.get('http://localhost:9000/people');
+//
+//    SpringDataRestAdapter.process(httpPromise).then(function (processedResponse) {
+//        $scope.person = processedResponse._embeddedItems;
+//    });
+
+    $http.get('http://localhost:9000/people/57093c6362b545f3835accaa')
+        .success(function (result) {
+            console.log(result);
+            $scope.song = result;
+
+        })
+        .error(function (data, status) {
+
+            console.log(data);
+
+        });
+
+
+//    $scope.addPerson = function () {
+//        $http.post('http://localhost:8080/people', { firstName: $scope.firstName, lastName: $scope.lastName })
+//            .success(function (result) {
+//
+//                console.log(result);
+//                $scope.people = result;
+//
+//            })
+//            .error(function (data, status) {
+//
+//                console.log(data);
+//
+//            });
+//    };
+//    
+    $scope.addSong = function () {
+        $http.post('http://localhost:9000/song', { title: $scope.title, artist: $scope.artist, bpm: $scope.bpm, key: $scope.key })
+            .success(function (result) {
+
+                console.log(result);
+                $scope.song = result;
+
+            })
+            .error(function (data, status) {
+
+                console.log(data);
+
+            });
+
+            $scope.title = '';
+            $scope.artist = '';
+            $scope.bpm = '';
+            $scope.key = '';
+    };
+    
+    
+    
+}]);
